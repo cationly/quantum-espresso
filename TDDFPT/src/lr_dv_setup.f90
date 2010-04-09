1722f1c1abc64ec5bbebb6cec977e8b0e2df3637
obm <obm>
2010-04-09 12:52:26 +0000

    RC 2 of TDDFPT

---
+++ TDDFPT/src/lr_dv_setup.f90
@@ -0,0 +1,81 @@
+!
+! Copyright (C) 2001 PWSCF group
+! This file is distributed under the terms of the
+! GNU General Public License. See the file `License'
+! in the root directory of the present distribution,
+! or http://www.gnu.org/copyleft/gpl.txt .
+!
+
+! Adapted to TDDFPT by Osman Baris Malcioglu (2009)
+!-----------------------------------------------------------------------
+subroutine lr_dv_setup
+  !-----------------------------------------------------------------------
+  !
+  !  This subroutine prepares some variable which is needed for derivatives
+  !  1) Set non linear core correction stuff 
+  !  2) computes dmuxc 3) with GC if needed
+  !
+#include "f_defs.h"
+  !
+  USE kinds,         ONLY : DP
+  USE ions_base,     ONLY : ntyp => nsp
+  USE lsda_mod,      ONLY : nspin, lsda
+  USE scf,           ONLY : rho, rho_core
+  USE gvect,         ONLY : nrxx, ngm
+  ! USE atom,          ONLY : nlcc
+  USE uspp_param,    ONLY : upf
+  !USE lr_variables,  ONLY : dmuxc, nlcc_any
+  USE nlcc_ph,       ONLY : nlcc_any
+  USE eqv,           ONLY : dmuxc
+  USE funct,         ONLY : dmxc, dmxc_spin  
+  USE lr_variables,  ONLY : lr_verbosity
+  USE io_global,     ONLY : stdout
+ implicit none
+  !
+  real(DP) :: rhotot, rhoup, rhodw
+  ! total charge
+  ! total up charge
+  ! total down charge
+  !
+  integer :: nt, ir
+  ! counter on mesh points
+  !
+  If (lr_verbosity > 5) THEN
+    WRITE(stdout,'("<lr_dv_setup>")')
+  endif
+  call start_clock ('lr_dv_setup')
+  !
+  ! 1) Set non linear core correction stuff
+  !
+  nlcc_any = ANY ( upf(1:ntyp)%nlcc )
+  !do nt = 1, ntyp
+  !   nlcc_any = nlcc_any.or.nlcc (nt)
+  !enddo
+  !
+  ! 2) Computes the derivative of the xc potential
+  !
+  dmuxc(:,:,:) = 0.0D0
+  if (lsda) then
+     do ir = 1, nrxx
+        rhoup = rho%of_r (ir, 1) + 0.5d0 * rho_core (ir)
+        rhodw = rho%of_r (ir, 2) + 0.5d0 * rho_core (ir)
+        call dmxc_spin (rhoup, rhodw, dmuxc(ir,1,1), dmuxc(ir,2,1), &
+                                      dmuxc(ir,1,2), dmuxc(ir,2,2) )
+     enddo
+  else
+     do ir = 1, nrxx
+        rhotot = rho%of_r (ir, 1) + rho_core (ir)
+        if (rhotot.gt.1.d-30) dmuxc (ir, 1, 1) = dmxc (rhotot)
+        if (rhotot.lt.-1.d-30) dmuxc (ir, 1, 1) = -dmxc ( -rhotot)
+     enddo
+  endif
+  !
+  ! 3) Setup all gradient correction stuff
+  !
+  call lr_setup_dgc()
+  !
+  if (lr_verbosity > 5) WRITE(stdout,'("<end of lr_dv_setup>")') 
+  call stop_clock ('lr_dv_setup')
+  !
+  return
+end subroutine lr_dv_setup
---
+++ TDDFPT/src/lr_h_psiq.f90
@@ -0,0 +1,289 @@
+!
+! Copyright (C) 2001-2007 Quantum-ESPRESSO group
+! This file is distributed under the terms of the
+! GNU General Public License. See the file `License'
+! in the root directory of the present distribution,
+! or http://www.gnu.org/copyleft/gpl.txt .
+!
+!
+!-----------------------------------------------------------------------
+subroutine lr_h_psiq (lda, n, m, psi, hpsi, spsi)
+  !-----------------------------------------------------------------------
+  !
+  !
+  ! Modified by Osman Baris Malcioglu (2009)
+  !
+  !     This routine computes the product of the Hamiltonian
+  !     and of the S matrix with a m  wavefunctions  contained
+  !     in psi. It first computes the bec matrix of these
+  !     wavefunctions and then with the routines hus_1psi and
+  !     s_psi computes for each band the required products
+  !
+
+  USE kinds,  ONLY : DP
+  USE wavefunctions_module,  ONLY : psic, psic_nc
+  USE noncollin_module, ONLY : noncolin, npol
+  USE lsda_mod, ONLY : current_spin
+  USE gsmooth,  ONLY : nls, nr1s, nr2s, nr3s, nrx1s, nrx2s, nrx3s, nrxxs
+  USE spin_orb, ONLY : domag
+  USE scf,    ONLY : vrs
+  USE uspp,   ONLY : vkb
+  USE wvfct,  ONLY : g2kin,igk
+  USE lr_variables,   ONLY : lr_verbosity
+  use control_flags,         only : gamma_only
+  use io_global,            only : stdout
+  !USE qpoint, ONLY : igkq
+  implicit none
+  !
+  !     Here the local variables
+  !
+  integer :: ibnd
+  ! counter on bands
+
+  integer :: lda, n, m
+  ! input: the leading dimension of the array psi
+  ! input: the real dimension of psi
+  ! input: the number of psi to compute
+  integer :: j
+  ! do loop index
+
+  complex(DP) :: psi (lda*npol, m), hpsi (lda*npol, m), spsi (lda*npol, m)
+  complex(DP) :: sup, sdwn
+  ! input: the functions where to apply H and S
+  ! output: H times psi
+  ! output: S times psi (Us PP's only)
+  !OBM debug
+  !real(DP) :: obm_debug
+  !complex(kind=dp), external :: ZDOTC
+ 
+
+  call start_clock ('h_psiq')
+  If (lr_verbosity > 5) WRITE(stdout,'("<lr_h_psiq>")')
+  if (gamma_only) then
+   call lr_h_psiq_gamma()
+  else
+   call lr_h_psiq_k()
+  endif
+  call stop_clock ('h_psiq')
+  return
+contains 
+!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!
+!k point part
+!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!
+    subroutine lr_h_psiq_k()
+
+    USE becmod, ONLY : bec_type, becp, calbec
+
+    IMPLICIT NONE
+  call start_clock ('init')
+
+  call calbec ( n, vkb, psi, becp, m)
+  !
+  ! Here we apply the kinetic energy (k+G)^2 psi
+  !
+  hpsi=(0.d0,0.d0)
+  do ibnd = 1, m
+     do j = 1, n
+        hpsi (j, ibnd) = g2kin (j) * psi (j, ibnd)
+     enddo
+  enddo
+  IF (noncolin) THEN
+     DO ibnd = 1, m
+        DO j = 1, n
+           hpsi (j+lda, ibnd) = g2kin (j) * psi (j+lda, ibnd)
+        ENDDO
+     ENDDO
+  ENDIF
+  call stop_clock ('init')
+  !
+  ! the local potential V_Loc psi. First the psi in real space
+  !
+
+  do ibnd = 1, m
+     call start_clock ('firstfft')
+     IF (noncolin) THEN
+        psic_nc = (0.d0, 0.d0)
+        do j = 1, n
+           psic_nc(nls(igk(j)),1) = psi (j, ibnd)
+           psic_nc(nls(igk(j)),2) = psi (j+lda, ibnd)
+        enddo
+        call cft3s (psic_nc(1,1), nr1s, nr2s, nr3s, nrx1s, nrx2s, nrx3s, 2)
+        call cft3s (psic_nc(1,2), nr1s, nr2s, nr3s, nrx1s, nrx2s, nrx3s, 2)
+     ELSE
+        psic(:) = (0.d0, 0.d0)
+        do j = 1, n
+           psic (nls(igk(j))) = psi (j, ibnd)
+        enddo
+        call cft3s (psic, nr1s, nr2s, nr3s, nrx1s, nrx2s, nrx3s, 2)
+     END IF
+     call stop_clock ('firstfft')
+     !
+     !   and then the product with the potential vrs = (vltot+vr) on the smoo
+     !
+     if (noncolin) then
+        if (domag) then
+           do j=1, nrxxs
+              sup = psic_nc(j,1) * (vrs(j,1)+vrs(j,4)) + &
+                    psic_nc(j,2) * (vrs(j,2)-(0.d0,1.d0)*vrs(j,3))
+              sdwn = psic_nc(j,2) * (vrs(j,1)-vrs(j,4)) + &
+                    psic_nc(j,1) * (vrs(j,2)+(0.d0,1.d0)*vrs(j,3))
+              psic_nc(j,1)=sup
+              psic_nc(j,2)=sdwn
+           end do
+        else
+           do j=1, nrxxs
+              psic_nc(j,1)=psic_nc(j,1) * vrs(j,1)
+              psic_nc(j,2)=psic_nc(j,2) * vrs(j,1)
+           enddo
+        endif
+     else
+        do j = 1, nrxxs
+           psic (j) = psic (j) * vrs (j, current_spin)
+        enddo
+     endif
+     !
+     !   back to reciprocal space
+     !
+     call start_clock ('secondfft')
+     IF (noncolin) THEN
+        call cft3s(psic_nc(1,1),nr1s,nr2s,nr3s,nrx1s,nrx2s,nrx3s,-2)
+        call cft3s(psic_nc(1,2),nr1s,nr2s,nr3s,nrx1s,nrx2s,nrx3s,-2)
+     !
+     !   addition to the total product
+     !
+        do j = 1, n
+           hpsi (j, ibnd) = hpsi (j, ibnd) + psic_nc (nls(igk(j)), 1)
+           hpsi (j+lda, ibnd) = hpsi (j+lda, ibnd) + psic_nc (nls(igk(j)), 2)
+        enddo
+     ELSE
+        call cft3s (psic, nr1s, nr2s, nr3s, nrx1s, nrx2s, nrx3s, - 2)
+     !
+     !   addition to the total product
+     !
+        do j = 1, n
+           hpsi (j, ibnd) = hpsi (j, ibnd) + psic (nls(igk(j)))
+        enddo
+     END IF
+     call stop_clock ('secondfft')
+  enddo
+  !
+  !  Here the product with the non local potential V_NL psi
+  !
+
+  call add_vuspsi (lda, n, m, hpsi)
+
+  call s_psi (lda, n, m, psi, spsi)
+
+    end subroutine lr_h_psiq_k
+!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!
+!gamma point part
+!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!
+    subroutine lr_h_psiq_gamma()
+
+    USE becmod, ONLY : becp, calbec
+    USE gvect,  ONLY : gstart
+    USE realus, ONLY : real_space, fft_orbital_gamma, &
+                       bfft_orbital_gamma, calbec_rs_gamma, add_vuspsir_gamma, &
+                       v_loc_psir, s_psir_gamma, real_space_debug
+    use uspp,                  only : nkb
+
+    IMPLICIT NONE
+    
+    call start_clock ('init')
+    !
+    ! Here we apply the kinetic energy (k+G)^2 psi
+    !
+    if(gstart==2) psi(1,:)=cmplx(real(psi(1,:),dp),0.0d0,dp)
+    ! 
+    !!OBM debug
+    !  obm_debug=0
+    !  do ibnd=1,m
+    !     !
+    !     obm_debug=obm_debug+ZDOTC(lda*npol,psi(:,ibnd),1,psi(:,ibnd),1)
+    !     !
+    !  enddo
+    !  print *, "lr_h_psiq psi", obm_debug
+    !!obm_debug
+
+    do ibnd=1,m
+       do j=1,n
+          hpsi(j,ibnd)=g2kin(j)*psi(j,ibnd)
+       enddo
+    enddo 
+    !!OBM debug
+    !  obm_debug=0
+    !  do ibnd=1,m
+    !     !
+    !     obm_debug=obm_debug+ZDOTC(lda*npol,hpsi(:,ibnd),1,hpsi(:,ibnd),1)
+    !     !
+    !  enddo
+    !  print *, "lr_h_psiq hpsi (just after kinetic operator)", obm_debug
+    !!obm_debug
+
+    call stop_clock ('init')
+      if (nkb > 0 .and. real_space_debug>2) then
+        do ibnd=1,m,2
+          !call check_fft_orbital_gamma(psi,ibnd,m)
+          call fft_orbital_gamma(psi,ibnd,m,.true.) !transform the psi real space, saved in temporary memory
+          call calbec_rs_gamma(ibnd,m,becp%r) !rbecp on psi
+          call s_psir_gamma(ibnd,m) !psi -> spsi
+          call bfft_orbital_gamma(spsi,ibnd,m) !return back to real space
+          call fft_orbital_gamma(hpsi,ibnd,m) ! spsi above is now replaced by hpsi
+          call v_loc_psir(ibnd,m) ! hpsi -> hpsi + psi*vrs  (psi read from temporary memory)
+          call add_vuspsir_gamma(ibnd,m) ! hpsi -> hpsi + vusp
+          call bfft_orbital_gamma(hpsi,ibnd,m,.true.) !transform back hpsi, clear psi in temporary memory
+        enddo
+     ELSE
+    call vloc_psi_gamma(lda,n,m,psi,vrs(1,current_spin),hpsi)
+    !!OBM debug
+    !  obm_debug=0
+    !  do ibnd=1,m
+    !     !
+    !     obm_debug=obm_debug+ZDOTC(lda*npol,hpsi(:,ibnd),1,hpsi(:,ibnd),1)
+    !     !
+    !  enddo
+    !  print *, "lr_h_psiq hpsi (after vloc_psi)", obm_debug
+    !!obm_debug
+
+     IF (noncolin) THEN
+       call errore ("lr_h_psiq","gamma and noncolin not implemented yet",1)
+     ELSE
+        call calbec ( n, vkb, psi, becp, m)
+     END IF
+     !!OBM debug
+     ! obm_debug=0
+     ! do ibnd=1,m
+     !    !
+     !    obm_debug=obm_debug+ZDOTC(lda*npol,becp%r(:,ibnd),1,becp%r(:,ibnd),1)
+     !    !
+     ! enddo
+     ! print *, "lr_h_psiq becp", obm_debug
+     !!obm_debug
+
+    
+     call add_vuspsi (lda, n, m, hpsi)
+     !END IF 
+     !!OBM debug
+     !  obm_debug=0
+     !  do ibnd=1,m
+     !     !
+     !     obm_debug=obm_debug+ZDOTC(lda*npol,hpsi(:,ibnd),1,hpsi(:,ibnd),1)
+     !     !
+     !  enddo
+     !  print *, "lr_h_psiq hpsi (after add_vuspsi)", obm_debug
+     !!obm_debug
+
+     call s_psi (lda, n, m, psi, spsi)
+     !!OBM debug
+     !  obm_debug=0
+     !  do ibnd=1,m
+     !     !
+     !     obm_debug=obm_debug+ZDOTC(lda*npol,spsi(:,ibnd),1,spsi(:,ibnd),1)
+     !     !
+     !  enddo
+     !  print *, "lr_h_psiq spsi ", obm_debug
+    !!obm_debug
+    ENDIF
+    end subroutine lr_h_psiq_gamma
+
+end subroutine lr_h_psiq
